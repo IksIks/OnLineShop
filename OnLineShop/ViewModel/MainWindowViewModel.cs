714a924603810d9e4a8c0dbfaf5157fe17930af7
iksiks30 <iksiks30@gmail.com>
2023-03-17 20:37:50 +0300

    Добавлены предварительные пункты меню и разметка

---
+++ OnLineShop/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using OnLineShop.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnLineShop.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+
+
+
+
+    }
+}
