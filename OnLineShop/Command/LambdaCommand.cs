def7ce4f407b94cf10f9896919cf8f235f93af0c
iksiks30 <iksiks30@gmail.com>
2023-03-18 14:21:33 +0300

    Реализовано соединение с базой данных клиентов

--- OnLineShop/Command/LambdaCommand.cs
+++ OnLineShop/Command/LambdaCommand.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace OnLineShop.Command
 {
-    internal class LambdaCommand
+    internal class LambdaCommand : CommandBase
     {
+
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecuted = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecuted;
+        }
+        public override bool CanExecute(object parameter)
+        {
+           return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter) => execute(parameter);
     }
 }
--- OnLineShop/ViewModel/MainWindowViewModel.cs
+++ OnLineShop/ViewModel/MainWindowViewModel.cs
@@ -1,29 +1,75 @@
-﻿using OnLineShop.ViewModel.Base;
+﻿using OnLineShop.Command;
+using OnLineShop.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OnLineShop.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        private string testConntection;
+        private SqlConnection connectionClientsDB;
+        private string clienBaseColorStatus, productBaseColorStatus = "Red";
 
-        public string ClienBaseColorStatus { get; set; } = "Red";
-        public string ProductBaseColorStatus { get; set; } = "Red";
+
+        public string TestConntection
+        {
+            get => testConntection;
+            set => Set(ref testConntection, value);
+        }
+        public string ClienBaseColorStatus
+        {
+            get => clienBaseColorStatus;
+            set => Set(ref clienBaseColorStatus, value);
+        }
+
+        public string ProductBaseColorStatus
+        {
+            get => productBaseColorStatus;
+            set => Set(ref productBaseColorStatus, value);
+        }
+
+
+        #region Команды
+
+        #region Команда загрузки базы
+        public ICommand ConnetcClientDBCommand { get; }
+        private bool CanConnetcClientDBCommandExecute(object parameter) => true;
+        private void OnConnetcClientDBCommandExecuted(object parameter)
+        {
+            connectionClientsDB.Open();
+            ClienBaseColorStatus = "Green";
+            TestConntection = connectionClientsDB.State.ToString();
+        }  
+        #endregion
+
+        #endregion
+
+
+
 
         public MainWindowViewModel()
         {
-            SqlConnectionStringBuilder connectionDBClients = new SqlConnectionStringBuilder()
+
+            SqlConnectionStringBuilder connectionStringClientsDB = new SqlConnectionStringBuilder()
             {
-                
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                InitialCatalog = "ClientsDB",
+                IntegratedSecurity = false
             };
 
+            connectionClientsDB = new SqlConnection()
+            {
+                ConnectionString = connectionStringClientsDB.ConnectionString
+            };
 
+            ConnetcClientDBCommand = new LambdaCommand(OnConnetcClientDBCommandExecuted, CanConnetcClientDBCommandExecute);
         }
-
 
 
 
